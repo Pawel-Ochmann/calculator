05ae10bcf16a814b137fac5f9579a10ff49233de
Pawel Ochmann <pawelochmann@poczta.onet.pl>
2023-03-31 18:51:35 +0200

    Chose background image, create basic layout withought mechanics behind it, needs much more improvements, hover effects, etc

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
+//variables
+
+let number1 = 2;
+let number2 = 4;
+let operator =2;
+
+
+
+//functions
+function operate(op,nr1,nr2) {
+    const operators = [add,subtract,multiply,divide];
+    return operators[op](nr1,nr2)
+}
+
+
 function add(a,b) {
     return a+b;
 };
@@ -11,3 +26,4 @@ function divide(a,b) {
     return a/b;
 }
 
+console.log(operate(operator,number1,number2));
