70d8f561462d5fae23ef45456c46513c0481b742
Pawel Ochmann <pawelochmann@poczta.onet.pl>
2023-04-03 10:57:39 +0200

    Create function that making 'equal' button works. Also it fires when operator is chosen and another is pressed (if two numbers are set up). The result is in the 'toFixed' function. And there is protection against dividing by '0'. Need to check everything, add function to backspace button, add proper devimals behaviour, and minus sign behaviour

--- main.js
+++ main.js
@@ -1,13 +1,18 @@
 //variables
 
-let number1 = 2;
-let number2 = 4;
-let actualOperator = '';
+let number1 = null;
+let number2 = null;
+let actualOperator = null;
 
 let display = document.querySelector('.display');
 
 const numbers = document.querySelectorAll('.number');
 const asideButtons = document.querySelectorAll('.aside_button')
+
+const clear = document.querySelector('#clear');
+const backspace = document.querySelector('#backspace');
+
+const equal = document.querySelector('#equal');
 
 //event-listeners
 numbers.forEach((number)=> {
@@ -18,11 +23,29 @@ asideButtons.forEach((button)=> {
 
 });
 
+clear.addEventListener('click', clearUp)
+
+equal.addEventListener('click', setUpOperate);
+
+const buttons = document.querySelectorAll('button');
+
+buttons.forEach((button)=> {
+    button.addEventListener('mousedown', ()=> {
+        button.classList.add('focus');
+    })
+    button.addEventListener('mouseup', ()=> {
+        button.classList.remove('focus');
+    })
+})
+
+
 
 //functions
 function operate(op,nr1,nr2) {
     const operators = [add,subtract,multiply,divide];
-    return operators[op](nr1,nr2)
+    let operationProduct =  operators[op](nr1,nr2).toFixed(10);
+    clearUp()
+    display.textContent = operationProduct;
 }
 
 
@@ -36,7 +59,8 @@ function multiply(a,b) {
     return a*b;
 }
 function divide(a,b) {
-    return a/b;
+    if (b===0) { return 'Do NOT divide by "0"!'}
+    else return a/b;
 }
 
 
@@ -44,9 +68,23 @@ function displayContent() {
     display.textContent += this.textContent;
 };
 function displayOperator() {
-    if (actualOperator) return; 
-    number1 = display.textContent;
-    console.log(+number1);
-    display.textContent += this.textContent;
-    actualOperator = this.dataset.op;
+    setUpOperate()
+    number1 = +display.textContent;
+    if (!number1) return
+    display.textContent += ` ${this.textContent} `;
+    actualOperator = +this.dataset.op;
+}
+
+function setUpOperate() {
+    const displaySecond = display.textContent.split(' ');
+    number2 = +displaySecond[2];
+    if (!number2 && number2!==0) return
+    operate(actualOperator,number1,number2)
+}
+
+function clearUp() {
+    display.textContent = '';
+    number1 = null;
+    number2 = null;
+    actualOperator = null;
 }
